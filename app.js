1736691cf6285a722620a8e48bf6938e79e555b2
David Calderon <calderon.djorge@gmail.com>
2018-05-25 23:41:17 +0000

    form para correo

--- app.js
+++ app.js
@@ -1,11 +1,16 @@
 var express = require("express");
 var app = express();
+var bodyParser = require("body-parser");
 var port = process.env.PORT || "8080";
 var ip = process.env.IP || "159.203.13.74";
 
+var api_key = 'key-b48534b7a46ccbceda643ad01b41f1a9';
+var domain = 'mail.temazcal.info';
+var mailgun = require('mailgun-js')({apiKey: api_key, domain: domain});
 
 app.set("view engine", "ejs");
 app.use(express.static(__dirname + "/public"));
+app.use(bodyParser.urlencoded({extended: true}));
 
 
 app.get("/", function(req, res){
@@ -26,6 +31,32 @@ app.get("/pago_exitoso", function(req, res){
 
 app.get("/pago_en_proceso", function(req, res){
     res.render("pago-en-proceso");
+});
+
+app.get("/client_info", function(req, res){
+    res.render("client-info");
+});
+
+app.post("/client_info", function(req, res){
+    var name = req.body.name;
+    var email = req.body.email;
+    var phone = req.body.phone;
+    console.log(email + name + phone);
+    
+    var mailData = {
+          from: 'Registros de Temazcal <servidor@temazcal.info>',
+          to: 'hola@temazcal.info',
+          subject: 'Registro en temazcal.info',
+          text: 'Un nuevo registro: \n\n' +
+          name + '\n\n' + email + '\n\n' + phone +
+          '\n'
+        };
+        
+        mailgun.messages().send(mailData, function (error, body) {
+          console.log(body);
+        });
+    
+    res.render("client-info");
 });
 
 app.get("*", function(req, res){
